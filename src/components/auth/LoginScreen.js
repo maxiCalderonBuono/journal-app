69aa9547924cf2d5c1924fe34dcfb6a8032464bd
Maximiliano <calderonmaxi@outlook.com>
2022-02-19 12:58:04 -0300

    feat: added sign-im  with google and email&password and user registration

--- src/components/auth/LoginScreen.js
+++ src/components/auth/LoginScreen.js
@@ -1,18 +1,77 @@
 import React from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
+import { login, startGoogleLogin } from "../../actions/auth";
+import { useForm } from "../../hooks/useForm";
+import validator from "validator";
+import { setUiError, removeUiError } from "../../actions/ui";
+import { startLoginWithEmailPassword} from "../../actions/auth";
 
 const LoginScreen = () => {
+  const dispatch = useDispatch();
+
+  const { msgError } = useSelector((state) => state.ui);
+
+  const [formValues, handleInputChange] = useForm({
+    email: "calderonmaxi@outlook.com",
+    password: "1234",
+  });
+
+  const { email, password } = formValues;
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (isFormValid()) {
+      dispatch(startLoginWithEmailPassword(email,password))
+    }
+  };
+
+  const handleGoogleLogin = () => {
+    dispatch(startGoogleLogin());
+  };
+
+  const isFormValid = () => {
+    if (!validator.isEmail(email)) {
+      dispatch(setUiError("Please, introduce a valid email"));
+      return false;
+    } else if (password.length < 6) {
+      dispatch(setUiError("Password must be at least 6 characters long"));
+      return false;
+    }
+
+    dispatch(removeUiError());
+    return true;
+  };
+
   return (
     <>
-      <form>
-        <h3 className= "auth__title mb-5">Login</h3>
-        <input className="auth__input" type="text" placeholder="Email" name="email" autoComplete= "off"/>
-        <input className="auth__input" type="password" placeholder="Password" name="password" />
-        <button className= "btn btn-primary btn-block" type="submit">Login</button>
-        
-        <div className= "auth__social-networks">
+      <form onSubmit={handleSubmit}>
+        <h3 className="auth__title mb-5">Login</h3>
+        {msgError && <div className="auth__alert-error">{msgError}</div>}
+        <input
+          className="auth__input"
+          type="text"
+          placeholder="Email"
+          name="email"
+          value={email}
+          onChange={handleInputChange}
+          autoComplete="off"
+        />
+        <input
+          className="auth__input"
+          type="password"
+          placeholder="Password"
+          name="password"
+          value={password}
+          onChange={handleInputChange}
+        />
+        <button className="btn btn-primary btn-block" type="submit">
+          Login
+        </button>
+
+        <div className="auth__social-networks">
           <p>Login with social networks</p>
-          <div className="google-btn">
+          <div className="google-btn" onClick={handleGoogleLogin}>
             <div className="google-icon-wrapper">
               <img
                 className="google-icon"
@@ -25,7 +84,9 @@ const LoginScreen = () => {
             </p>
           </div>
         </div>
-        <Link className= "link" to="/auth/register">Create new account</Link>
+        <Link className="link" to="/auth/register">
+          Create new account
+        </Link>
       </form>
     </>
   );
--- src/components/auth/RegisterScreen.js
+++ src/components/auth/RegisterScreen.js
@@ -1,19 +1,177 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useForm } from "../../hooks/useForm";
+import validator from "validator";
+import { useDispatch, useSelector } from "react-redux";
+import { setUiError, removeUiError } from "../../actions/ui";
+import { registerWithEmailPassword } from "../../actions/auth";
+
 
 const RegisterScreen = () => {
+  const initialUser = {
+    nick: "Emc",
+    name: "Albert",
+    lastname: "Einstein",
+    email: "mimail@gmail.com",
+    password: "123456",
+    password2: "123456",
+    dni: 33333333,
+    birthday: "1991-08-12",
+    phone: "2618546239",
+  };
+
+  const [formValues, handleInputChange] = useForm(initialUser);
+
+  const {
+    nick,
+    name,
+    lastname,
+    email,
+    password,
+    password2,
+    dni,
+    birthday,
+    phone,
+  } = formValues;
+
+  const dispatch = useDispatch();
+  const { msgError } = useSelector((state) => state.ui);
+
+  const handleRegister = (e) => {
+    e.preventDefault();
+
+    if (isFormValid()) {
+      dispatch(registerWithEmailPassword(email,password, name))
+    }
+  };
+
+  const isFormValid = () => {
+    // validator.isEmpty(name , { ignore_whitespace: true })
+    if (name.trim().length < 2) {
+      dispatch(setUiError("Please, introduce a complete name"));
+      return false;
+    } else if (lastname.trim().length < 2) {
+      dispatch(setUiError("Please, introduce a complete surname"));
+      return false;
+    } else if (!validator.isEmail(email)) {
+      dispatch(setUiError("Please, introduce a valid email"));
+      return false;
+    } else if (password !== password2 || password.length < 6) {
+      dispatch(setUiError("Password must be at least 6 characters long"));
+      return false;
+    } else if (validator.isAfter(birthday)) {
+      dispatch(setUiError("Date cannot be equal or greater than current day"));
+      return false;
+    } else if (validator.isEmpty(dni.toString(), { ignore_whitespace: true })) {
+      dispatch(setUiError("ID muts be a valid combinaion of numbers"));
+      return false;
+    }
+
+    dispatch(removeUiError());
+    return true;
+  };
+
   return (
     <>
-      <form>
-        <h3 className= "auth__title mb-5">Register</h3>
-        <input className="auth__input" type="text" placeholder="Name" name="name" autoComplete= "off"/>
-        <input className="auth__input" type="text" placeholder="Email" name="email" autoComplete= "off"/>
-        <input className="auth__input" type="password" placeholder="Password" name="password" />
-        <input className="auth__input" type="password" placeholder="Confirm password" name="password2" />
-        <button className= "btn btn-primary btn-block mb-5" type="submit">Register</button>
-        
-      
-        <Link className= "link" to="/auth/login">Already registered?</Link>
+      <form onSubmit={handleRegister}>
+        <h3 className="auth__title mb-5">Register</h3>
+
+        {msgError && <div className="auth__alert-error">{msgError}</div>}
+        <input
+          className="auth__input"
+          type="text"
+          placeholder="Nick"
+          name="nick"
+          autoComplete="off"
+          onChange={handleInputChange}
+          value={nick}
+          required
+        />
+        <input
+          className="auth__input"
+          type="text"
+          placeholder="Name"
+          name="name"
+          autoComplete="off"
+          onChange={handleInputChange}
+          value={name}
+          required
+        />
+        <input
+          className="auth__input"
+          type="text"
+          placeholder="LastName"
+          name="lastname"
+          autoComplete="off"
+          onChange={handleInputChange}
+          value={lastname}
+          required
+        />
+        <input
+          className="auth__input"
+          type="email"
+          placeholder="Email"
+          name="email"
+          autoComplete="off"
+          onChange={handleInputChange}
+          value={email}
+          required
+        />
+        <input
+          className="auth__input"
+          type="password"
+          placeholder="Password"
+          name="password"
+          onChange={handleInputChange}
+          value={password}
+          required
+        />
+        <input
+          className="auth__input"
+          type="password"
+          placeholder="Repeat your password"
+          name="password2"
+          onChange={handleInputChange}
+          value={password2}
+          required
+        />
+        <input
+          className="auth__input"
+          type="number"
+          placeholder="DNI"
+          name="dni"
+          autoComplete="off"
+          onChange={handleInputChange}
+          value={dni}
+          required
+        />
+        <input
+          className="auth__input"
+          type="date"
+          placeholder="Birthday"
+          name="birthday"
+          autoComplete="off"
+          onChange={handleInputChange}
+          value={birthday}
+          required
+        />
+        <input
+          className="auth__input"
+          type="tel"
+          placeholder="Phone"
+          name="phone"
+          onChange={handleInputChange}
+          value={phone}
+          required
+        />
+
+        <button className="btn btn-primary btn-block mb-5" type="submit">
+          Register
+        </button>
+
+        <Link className="link" to="/auth/login">
+          Already registered?
+        </Link>
       </form>
     </>
   );
