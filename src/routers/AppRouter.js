0c5138d19d495ee812a7ef41d0841e99d72d3038
Maximiliano <calderonmaxi@outlook.com>
2022-03-03 12:46:59 -0300

    feat: authentication remains after refresh using onAuthStateChanged

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,10 +1,22 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import JournalScreen from "../components/journal/JournalScreen";
 import AuthRouter from "./AuthRouter";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { useDispatch } from "react-redux";
+import { login } from "../actions/auth";
 
 const AppRouter = () => {
-  
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const auth = getAuth();
+    onAuthStateChanged(auth, (user) => {
+      if (user?.uid) {
+        dispatch(login(user.uid, user.displayName));
+      }
+    });
+  }, []);
 
   return (
     <Router>
